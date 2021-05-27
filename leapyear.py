02cf50af755267e652d2854408b54cb30dd5ea76
JacobEckroth <jacobeckroth@gmail.com>
2021-05-27 10:35:07 -0700

    implemented divis by 400 method where a number divided by 400 returns False

--- leapyear.py
+++ leapyear.py
@@ -2,6 +2,8 @@
 def isALeapYear(year):
     if(year % 4 == 0):
         if(year % 100 == 0):
+            if(year % 400 == 0):
+                return True
             return False
         return True
     return False
