cf6ab3b725948a42296c2e825b6ded574c0cdf7e
JacobEckroth <jacobeckroth@gmail.com>
2021-05-26 22:29:55 -0700

    wrote second case, testing if 3 gives fizz

--- fizzbussTest.py
+++ fizzbussTest.py
@@ -6,6 +6,9 @@ import unittest
 class fizzbuzzTestCase(unittest.TestCase):
     def test_normalValue(self):
         self.assertEqual(fizzbuzz.getFizzBuzzResult(1),"1")
+    def test_divisibleByThree(self):
+        self.assertEqual(fizzbuzz.getFizzBuzzResult(3),"Fizz")
+        
 
 
 if __name__ == "__main__":
