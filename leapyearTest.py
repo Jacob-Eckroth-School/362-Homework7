b97626230f298a667a6a055ad9eb848faeb07731
JacobEckroth <jacobeckroth@gmail.com>
2021-05-27 10:33:02 -0700

    wrote third test to check if a number divided by 100 is false

--- leapyearTest.py
+++ leapyearTest.py
@@ -6,5 +6,7 @@ class leapyearTestCase(unittest.TestCase):
         self.assertEqual(leapyear.isALeapYear(1),False)
     def test_divisByFour(self):
         self.assertEqual(leapyear.isALeapYear(4),True)
+    def test_divisBy100(self):
+        self.assertEqual(leapyear.isALeapYear(100),False)
 if __name__ == "__main__":
     unittest.main()
