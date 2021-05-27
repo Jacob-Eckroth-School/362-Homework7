e3163619e0948d4eb2b412f4cd21ebf15866cc68
JacobEckroth <jacobeckroth@gmail.com>
2021-05-27 10:31:11 -0700

    wrote second test to check that years divisible by 4 returns true

--- leapyearTest.py
+++ leapyearTest.py
@@ -4,5 +4,7 @@ import unittest
 class leapyearTestCase(unittest.TestCase):
     def test_normalyear(self):
         self.assertEqual(leapyear.isALeapYear(1),False)
+    def test_divisByFour(self):
+        self.assertEqual(leapyear.isALeapYear(4),True)
 if __name__ == "__main__":
     unittest.main()
