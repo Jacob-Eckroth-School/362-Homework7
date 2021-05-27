a674ec69945284ec2d826884948f5a3a53b9b020
JacobEckroth <jacobeckroth@gmail.com>
2021-05-26 22:34:57 -0700

    implemented fourth method that prints FizzBuzz if a number is divisible by 3 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 
 def getFizzBuzzResult(val):
-    if(val%3 == 0):
-        return "Fizz"
+    if(val%3 == 0 and val%5 == 0):
+        return "FizzBuzz"
     elif(val%5==0):
         return "Buzz"
+    elif(val%3==0):
+        return "Fizz"
     return str(val)
